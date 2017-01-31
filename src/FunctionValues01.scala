93c8617587d23c367dcb72b6c32fa86170d167b0
Mahesh Pujari <pujarimahesh_kumar@yahoo.com>
2017-01-31 09:40:16 +0530

    adding function values

--- src/FunctionValues01.scala
+++ src/FunctionValues01.scala
@@ -9,4 +9,15 @@ def fileMatching(query: String, matcher: (String, String) => Boolean ) = {
 
 def filesEnding(query: String) = fileMatching(query, _.endsWith(_))
 
+def filesContaining(query: String) = fileMatching(query, _.contains(_))
+
+def filesMatches(query: String) = fileMatching(query, _.matches(_))
+
+println("\nEnding with:\n")
 filesEnding("01.scala").foreach(println _)
+
+println("\n\nContains:\n")
+filesContaining("01").foreach(println _)
+
+println("\n\nMatches:\n")
+filesMatches(".*01.*").foreach(println _)
